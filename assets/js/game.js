175d16adf926160ffb628c407e6e9e15e7276d6c
BradD <bradleyeday@gmail.com>
2020-06-10 17:56:52 -0500

    while statement added

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,9 +13,12 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+window.alert("Welcome to Robot Gladiators!");
 
 var fight = function(enemyName) {
-    window.alert("Welcome to Robot Gladiators!");
+    
+    while(enemyHealth > 0) {
+        
 
     var promptFight = window.prompt("would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.")
     
@@ -60,6 +63,10 @@ var fight = function(enemyName) {
     } else {
         window.alert("You need to pick a valid option. Try again!")
     }
+
+    }
+    
+    
     
    
 
@@ -67,6 +74,8 @@ var fight = function(enemyName) {
     
 }
 
-for(var i = 0; i < enemyNames.length; i++) {
+for (var i = 0; i < enemyNames.length; i++) {
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
     fight(enemyNames[i]);
 }
