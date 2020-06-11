3435bb81bc72cc43ba67041cbf5b7fb8d5042517
BradD <bradleyeday@gmail.com>
2020-06-10 19:48:23 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -40,6 +40,7 @@ var fight = function(enemyName) {
         if (enemyHealth <=0) {
             window.alert(enemyName + " has died!");
             break;
+            // shop after fight
         }
         else {
             window.alert(enemyName + " still has " + enemyHealth + " health left.");
@@ -62,16 +63,50 @@ var fight = function(enemyName) {
     }  
 };
 
-for (var i = 0; i < enemyNames.length; i++) {
-    if (palyerHealth > 0) {
-        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1) );
+var endGame = function() {
     
-        var pickedEnemyName = enemyNames[i];
-        enemyHealth = 50;
-        fight(enemyNames[i]);
+    if (palyerHealth > 0) {
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+
     }
     else {
-        window.alert("You have lost your robot in battle! Game Over!");
-        break;
+        window.alert("You've lost your robot in battle.");
     }
-}
+
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+    if (playAgainConfirm) {
+        startGame();
+    }
+    else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
+};
+
+var startGame = function() {
+    palyerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+
+
+    for (var i = 0; i < enemyNames.length; i++) {
+        if (palyerHealth > 0) {
+            window.alert("Welcome to Robot Gladiators! Round " + ( i + 1) );
+    
+            var pickedEnemyName = enemyNames[i];
+            enemyHealth = 50;
+            fight(enemyNames[i]);
+        }
+        else {
+            window.alert("You have lost your robot in battle! Game Over!");
+            break;
+        }
+    }
+
+    endGame();
+};
+
+startGame();
+
+
+
