02de3a52fd57f2a9d7468034044718e761f0c45f
BradD <bradleyeday@gmail.com>
2020-06-10 20:22:48 -0500

    shop functionality

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,7 +5,7 @@
 // "LOSE" - Player robot's health is zero or less
 
 var playerName = window.prompt("What is your robot's name?");
-var palyerHealth = 100;
+var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
@@ -15,7 +15,7 @@ var enemyAttack = 12;
 
 var fight = function(enemyName) {
     
-    while(enemyHealth > 0 && palyerHealth > 0) {
+    while(enemyHealth > 0 && playerHealth > 0) {
         
 
     var promptFight = window.prompt("would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.")
@@ -46,18 +46,18 @@ var fight = function(enemyName) {
             window.alert(enemyName + " still has " + enemyHealth + " health left.");
         }
 
-        palyerHealth = palyerHealth - enemyAttack;
+        playerHealth = playerHealth - enemyAttack;
 
         console.log (
-        enemyName + "attacked" + playerName + "." + playerName + " now has " + palyerHealth + " health remaning."    
+        enemyName + "attacked" + playerName + "." + playerName + " now has " + playerHealth + " health remaning."    
         )
 
-        if (palyerHealth <=0) {
+        if (playerHealth <=0) {
         window.alert(playerName + " has died!");
         break;
         }
         else {
-        window.alert(playerName + " still has " + palyerHealth + " health left.");
+        window.alert(playerName + " still has " + playerHealth + " health left.");
         }
 
     }  
@@ -65,7 +65,7 @@ var fight = function(enemyName) {
 
 var endGame = function() {
     
-    if (palyerHealth > 0) {
+    if (playerHealth > 0) {
         window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
 
     }
@@ -83,19 +83,69 @@ var endGame = function() {
     }
 };
 
+var shop = function() {
+    var shopOptionPrompt = window.prompt(
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the shop? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    );
+    switch (shopOptionPrompt) {
+        case "REFILL":
+        case "refill":
+            if (playerMoney >= 7) {
+                window.alert("refilling player's health by 20 for 7 dollars.");
+
+                playerHealth = playerHealth + 20;
+                playerMoney = playerMoney -7;
+        } 
+        else {
+            window.alert("You don't have enough money!");
+        } 
+            break;
+        case "UPGRADE":    
+        case "upgrade":
+            if (playerMoney >= 7) {
+                window.alert("Upgrading player's attack by 6 for 7 dollars.");
+
+                playerAttack = playerAttack + 6;
+                palyerMoney = playerMoney -7;
+            }
+            else {
+                window.alert("you don't have enough money!")
+            }    
+            break;
+        case "LEAVE":    
+        case "leave":
+            window.alert("Leaving the store.");
+
+            break;
+        default:
+            window.alert("You did not pick a valid option. Try again.");
+
+            shop();
+            break;
+    }
+};
+
 var startGame = function() {
-    palyerHealth = 100;
+    playerHealth = 100;
     playerAttack = 10;
     playerMoney = 10;
 
 
     for (var i = 0; i < enemyNames.length; i++) {
-        if (palyerHealth > 0) {
+        if (playerHealth > 0) {
             window.alert("Welcome to Robot Gladiators! Round " + ( i + 1) );
     
             var pickedEnemyName = enemyNames[i];
             enemyHealth = 50;
-            fight(enemyNames[i]);
+            fight(pickedEnemyName);
+            if (playerHealth > 0 && i < enemyNames.length - 1) {
+                var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+
+                if(storeConfirm) {
+                    shop();
+                }
+            }
+        
         }
         else {
             window.alert("You have lost your robot in battle! Game Over!");
@@ -105,6 +155,8 @@ var startGame = function() {
 
     endGame();
 };
+
+
 
 startGame();
 
